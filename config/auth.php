fb674f071843c6ea0b25ca1c443f4a30151b9f8e
agoenxz22 <agoenxz22@gmail.com>
2025-12-30 21:42:38 +0700

    feat: add API authentication guard and user credential provider

--- config/auth.php
+++ config/auth.php
@@ -39,6 +39,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+         'api' => [
+            'driver' => 'sanctum',
+            'provider' => 'users_credential',
+            'hash' => false,
+        ],
     ],
 
     /*
@@ -64,6 +69,10 @@ return [
             'model' => env('AUTH_MODEL', App\Models\User::class),
         ],
 
+        'users_credential' => [
+            'driver' => 'eloquent',
+            'model' => env('AUTH_CREDENTIAL_MODEL', App\Models\UserCredential::class),
+        ],
         // 'users' => [
         //     'driver' => 'database',
         //     'table' => 'users',
@@ -92,6 +101,12 @@ return [
     'passwords' => [
         'users' => [
             'provider' => 'users',
+            'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
+            'expire' => 60,
+            'throttle' => 60,
+        ],
+         'users_credential' => [
+            'provider' => 'users_credential',
             'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
             'expire' => 60,
             'throttle' => 60,
